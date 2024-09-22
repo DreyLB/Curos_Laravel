03e153548bc95c778eda8a2ea33e32852a2af80e
Andrey <andreycesar02@gmail.com>
2024-09-21 23:06:08 -0300

    Refatorando tela de eventos

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
-use App\Http\Controllers\ProductController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
