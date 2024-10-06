a13c90757e3ec648f080275ff10fa7962f32b84c
Andrey <andreycesar02@gmail.com>
2024-10-05 22:35:01 -0300

    Salvando datas no banco de dados

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -7,46 +7,55 @@ use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $events = Event::all();
-        
+
         return view('welcome', ['events' => $events]);
     }
 
-    public function create(){
+    public function create()
+    {
         return view('events.create');
     }
 
-    public function store(Request $request){
+    //Envia dados ao banco de dados
+    public function store(Request $request)
+    {
 
         $event = new Event;
 
-        $event->title = $request->title;
-        $event->city = $request->city;
-        $event->private = $request->private;
+        $event->title       = $request->title;
+        $event->date        = $request->date;
+        $event->city        = $request->city;
+        $event->private     = $request->private;
         $event->description = $request->description;
-        $event->items = $request->items;
+        $event->items       = $request->items;
 
         // Image Upload
-        if($request->hasFile('image') && $request->file('image')->isValid()){
-            $requestImage = $request->image;
-            $extension = $requestImage->extension();
-            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage   = $request->image;
+            $extension      = $requestImage->extension();
+            $imageName      = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
             $requestImage->move(public_path('img/events'), $imageName);
-            $event->image = $imageName;
+
+            $event->image   = $imageName;
         }
 
+        // Salva todos os requests acima no banco de dados
         $event->save();
 
+        //Redireciona para a raiz "/" e envia uma mensagem "msg"
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
-
     }
 
-    public function show($id){
+    //Mostra os dados que EventController salvou
+    public function show($id)
+    {
         $event = Event::findOrFail($id);
 
         return view('events.show', ['event' => $event]);
     }
 }
-
--- hdcevents/resources/views/layouts/main.blade.php
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -57,6 +57,9 @@
         <footer>
             <p>HDC Events &copy; 2020</p>
         </footer>
+
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </body>
 </html>
 
